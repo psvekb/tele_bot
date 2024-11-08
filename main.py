8d75ae2a722d6e6f3f2016b698a27b2a4160f692
Sergej Pakhomov <psv.ekb@gmail.com>
2024-11-08 21:14:58 +0500

    second

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import math
 import pprint
 from typing import Tuple
-print('Hello, World!')
 
-def foo(x:int)->Tuple[int, float]:
+print("Hello, World!")
+
+
+def foo(x: int) -> Tuple[int, float]:
     return x, math.pi
 
-pprint.pprint('Hello')
+
+pprint.pprint("Hello ")
 
 pprint.pprint(foo(4))
