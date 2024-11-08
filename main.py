612e5414000caff882f3a3b871c66241b7fa1596
Sergej Pakhomov <psv.ekb@gmail.com>
2024-11-09 00:18:26 +0500

    bot-num

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-import math
-import pprint
-from typing import Tuple
-
-print("Hello, World!")
+import requests
+import time
 
 
-def foo(x: int) -> Tuple[int, float]:
-    return x, math.pi
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '7831609330:AAHn-UPoq4oMDrvMSgJL4dF8yWTgJMYLrTI'
+TEXT = 'Ура! Классный апдейт!'
+MAX_COUNTER = 100
 
+offset = -2
+counter = 0
+chat_id: int
 
-pprint.pprint("Hello ")
+while counter < MAX_COUNTER:
+    print ('attempt=', counter)
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}').json()
 
-pprint.pprint(foo(4))
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+    time.sleep(1)
+    counter +=1
